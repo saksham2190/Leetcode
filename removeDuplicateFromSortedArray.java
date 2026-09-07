9db005d749516316ebf81f8b7f25d500146c617a
saksham9140 <sakshamyadav9140@gmail.com>
2026-09-07 17:25:43 +0530

    removeDuplicateFromSortedArray

---
+++ removeDuplicateFromSortedArray.java
@@ -0,0 +1,12 @@
+class removeDuplicateFromSortedArray{
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1; j<=nums.length-1; j++){
+            if (nums[j] != nums[i]) {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+        return (i+1);
+    }
+}
