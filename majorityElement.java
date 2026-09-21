ec8106c65c74f3b803367fc4592d76f2849a07bb
saksham9140 <sakshamyadav9140@gmail.com>
2026-09-22 01:45:02 +0530

    majorityElement

---
+++ majorityElement.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int majorityElement(int []nums) {
+        HashMap <Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(map.containsKey(nums[i])){ //true
+                map.put(nums[i], map.get(nums[i]) + 1 );
+            }
+            else { // false
+                map.put(nums[i], 1);
+            }
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) >n/2){
+                return key;
+            }
+        }
+        return 0;
+        
+    }
+}
--- removeDuplicateFromSortedArray.java
+++ removeDuplicateFromSortedArray.java
@@ -1,4 +1,4 @@
-class removeDuplicateFromSortedArray{
+ class removeDuplicateFromSortedArray{
     public int removeDuplicates(int[] nums) {
         int i=0;
         for(int j=1; j<=nums.length-1; j++){
